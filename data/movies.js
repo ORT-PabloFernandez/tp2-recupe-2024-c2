a59e82ddb6ac12e52f67974d2ddf8e510cc297f9
Michael <michael.alexander.rios@gmail.com>
2024-11-07 21:53:00 -0300

    MICHAEL ALEXANDER RIOS OROSCO

--- data/movies.js
+++ data/movies.js
@@ -1,3 +1,4 @@
+import { ObjectId } from "mongodb";
 import getConnection from "./conn.js";
 const DATABASE = "sample_mflix";
 const MOVIES = "movies";
@@ -15,3 +16,32 @@ async function getAllMovies(pageSize, page) {
 }
 
 export { getAllMovies };
+
+
+export async function getMovieById(id){
+  const conectiondb = await getConnection();
+  const movie = conectiondb.db(DATABASE).collection(MOVIES).findOne({ _id: new ObjectId(id)});
+
+  return movie;
+}
+
+export async function getMoviesWinners(){
+  const connectiondb = await getConnection();
+  const movies = connectiondb.db(DATABASE).collection(MOVIES);
+
+  //const moviesWinners = await movies.find({"awards.wins": { $gt: 0 }},{projection: { title: 1, poster: 1 , plot: 1 }}).toArray();
+  const moviesWinners = await movies.find().toArray();
+
+  return moviesWinners;
+}
+
+
+export async function getMoviesByLanguaje(languaje) {
+  const connectiondb = await getConnection();
+  const movies = connectiondb
+    .db(DATABASE)
+    .collection(MOVIES)
+    .find({ languages: languaje })
+    .toArray();
+  return movies;
+}
--- routes/movies.js
+++ routes/movies.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { getAllMovies } from "../data/movies.js";
+import { getAllMovies, getMovieById, getMoviesWinners, getMoviesByLanguaje } from "../data/movies.js";
 
 const router = express.Router();
 
@@ -9,5 +9,31 @@ router.get("/", async (req, res) => {
 
   res.json(await getAllMovies(pageSize, page));
 });
+
+
+//> 1. Necesitamos un endpoint que nos devuelva una película (**movie**) particular por \_id
+router.get("/:id", async (req, res) => {
+  console.log("Id");
+  const movie = await getMovieById(req.params.id);
+  res.json(movie);
+})
+
+
+//> 2. Los desarrolladores de frontend estan haciendo un pantalla para mostrar solo las películas ganadoras de al menos un premio.
+//Necesitamos que desarrolles el endpoint respectivo. Solo necesitan el titulo, el poster y el resumen de la reseña (**plot**)
+router.get("/winners", async (req, res) => {
+  console.log("Peliculas con al menos un premio")
+  const winners = await getMoviesWinners();
+  res.json(winners);
+});
+
+//Necesitamos un endpoint que nos devuelva las peliculas filtradas por idioma. 
+//Toma en cuenta que estas películas pueden ser muchas y el desarrollador de frontend va mostrarlas paginadas.
+router.get("/byLanguage/:languaje", async (req, res) => {
+  console.log("Peliculas por lenguaje");
+  const movies = await getMoviesByLanguaje(req.params.languaje);
+  res.json(movies);
+});
+
 
 export default router;
